a3288521f88b59105edcffc3f74a61295b18ad13
involid <mher.karagulyan@gmail.com>
2023-10-20 23:33:23 +0300

    fixed

--- problem.cpp
+++ problem.cpp
@@ -6,10 +6,11 @@
 #include <openssl/evp.h>
 #include <string>
 #include <vector>
+#include <filesystem>
 using std::string;
 using std::vector;
 
-const int ChunkSize = (1 << 12);
+const size_t MaxChunkSize = (1 << 10);
 std::hash<string> hasher;
 
 size_t computeSHA256Hash(const string& data)
@@ -36,7 +37,7 @@ size_t computeSHA256Hash(const string& data)
     return hasher(hexHash);
 }
 
-vector<size_t> compute_hashes(const string& filePath)
+vector<size_t> compute_hashes(const string& filePath, size_t chunk_size = (1 << 12))
 {
     vector<size_t> hashes;
     std::ifstream file(filePath);
@@ -50,7 +51,7 @@ vector<size_t> compute_hashes(const string& filePath)
     char buffer;
     while (file.get(buffer)) {
         chunk += buffer;
-        if (chunk.length() >= ChunkSize) {
+        if (chunk.length() >= chunk_size) {
             size_t hash = computeSHA256Hash(chunk);
             hashes.push_back(hash);
             chunk.clear();
@@ -92,44 +93,28 @@ bool similar_files(const std::string& path1, const std::string& path2)
     return true;
 }
 
-bool very_different(const string& path1, const string& path2, int percentage)
-{
-    std::ifstream stream1(path1, std::ios::binary | std::ios::ate);
-    std::ifstream stream2(path2, std::ios::binary | std::ios::ate);
-
-    if (!stream1.is_open() || !stream2.is_open()) {
-        std::cerr << "Failed to open one of the files." << std::endl;
-        return false;
-    }
-
-    if (std::min(stream1.tellg(), stream2.tellg()) >=
-        percentage * std::max(stream1.tellg(), stream2.tellg())) {
-        return false;
-    }
-    return true;
-}
-
 size_t min(size_t a, size_t b, size_t c)
 {
     return std::min(std::min(a, b), c);
 }
 
-size_t levenshtein_distance(const vector<size_t>& s1, const vector<size_t>& s2)
+template <typename T>
+size_t levenshtein_distance(const T& s1, const T& s2)
 {
     size_t len1 = s1.size();
     size_t len2 = s2.size();
 
     vector<vector<size_t>> dp(len1 + 1, vector<size_t>(len2 + 1, 0));
 
-    for (size_t i = 0; i <= len1; i++) {
+    for (size_t i = 0; i <= len1; ++i) {
         dp[i][0] = i;
     }
-    for (size_t j = 0; j <= len2; j++) {
+    for (size_t j = 0; j <= len2; ++j) {
         dp[0][j] = j;
     }
 
-    for (size_t i = 1; i <= len1; i++) {
-        for (size_t j = 1; j <= len2; j++) {
+    for (size_t i = 1; i <= len1; ++i) {
+        for (size_t j = 1; j <= len2; ++j) {
             size_t cost = (s1[i - 1] == s2[j - 1]) ? 0 : 1;
             dp[i][j] = min(
                 dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + cost);
@@ -144,23 +129,23 @@ void find_similarities(
     const string& dir_path2,
     int sim_percentage)
 {
-    vector<vector<size_t>> hashes1;
-    vector<vector<size_t>> hashes2;
     vector<string> names1;
     vector<string> names2;
+    vector<size_t> sizes1;
+    vector<size_t> sizes2;
 
     // computing hashes for files and saving names.
     for (const auto& entry : std::filesystem::directory_iterator(dir_path1)) {
         if (std::filesystem::is_regular_file(entry)) {
             names1.push_back(entry.path().string());
-            hashes1.push_back(compute_hashes(names1.back()));
+            sizes1.push_back(file_size(entry));
         }
     }
 
     for (const auto& entry : std::filesystem::directory_iterator(dir_path2)) {
         if (std::filesystem::is_regular_file(entry)) {
             names2.push_back(entry.path().string());
-            hashes2.push_back(compute_hashes(names2.back()));
+            sizes2.push_back(file_size(entry));
         }
     }
 
@@ -174,20 +159,22 @@ void find_similarities(
         similarities[i].resize(names2.size());
         for (int j = 0; j < names2.size(); ++j) {
             similarities[i][j] = 0;
-            if (hashes1[i] == hashes2[j] &&
-                similar_files(names1[i], names2[j])) {
+            if (similar_files(names1[i], names2[j])) {
                 std::cout << names1[i] << " - " << names2[j]
                           << " files are identical." << std::endl;
                 found_similar1[i] = true;
                 found_similar2[j] = true;
                 continue;
             }
-            if (very_different(names1[i], names2[j], sim_percentage)) {
+            if (std::min(sizes1[i], sizes2[j]) * 100 < std::max(sizes1[i], sizes2[j]) * sim_percentage) {
                 continue;
             }
-            double perc = static_cast<double>(
-                              levenshtein_distance(hashes1[i], hashes2[j])) /
-                          std::max(hashes1[i].size(), hashes2[j].size()) * 100;
+            double perc;
+            size_t chunk_size = std::max(sizes1[i], sizes2[j]) / MaxChunkSize + 1;
+            auto hash1 = compute_hashes(names1[i], chunk_size);
+            auto hash2 = compute_hashes(names2[j], chunk_size);
+            perc = static_cast<double>(levenshtein_distance(hash1, hash2)) /
+                          std::max(hash1.size(), hash2.size()) * 100;
             similarities[i][j] = perc;
         }
     }
@@ -196,7 +183,7 @@ void find_similarities(
         for (int j = 0; j < names2.size(); ++j) {
             if (similarities[i][j] >= sim_percentage) {
                 std::cout << names1[i] << " - " << names2[j]
-                          << "  files are identical with" << similarities[i][j]
+                          << "  files are identical with " << similarities[i][j]
                           << "%" << std::endl;
                 found_similar1[i] = true;
                 found_similar2[j] = true;
@@ -212,7 +199,7 @@ void find_similarities(
     }
     for (int i = 0; i < names2.size(); ++i) {
         if (!found_similar2[i]) {
-            std::cout << dir_path2 << '/' << names2[i] << " is not in "
+            std::cout << names2[i] << " is not in "
                       << dir_path1 << std::endl;
         }
     }
@@ -220,7 +207,7 @@ void find_similarities(
 
 int main()
 {
-    int sim_percentage;
+    double sim_percentage;
     std::cin >> sim_percentage;
     string directory1;
     string directory2;
